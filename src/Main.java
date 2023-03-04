729a56f5fc14b6f267a57d752bac3392f47473fa
Miguel Rios <91401349+NeoMikhe@users.noreply.github.com>
2023-03-04 00:56:46 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -24,114 +24,31 @@ public class Main {
         System.out.println("Pancake desornado : "+pancakeList);
         System.out.println();
         System.out.println();
-        String[] A =  new String[5];
-        for(int i=0; i<pancakeList.size(); i++){
-            A[i]+= pancakeList.get(i);
-        }
-        System.out.println(BFS(A[1]));
-        
-        /*
-        // Ciclo que ordena la ArrayList Pancake siempre del mayor hasta el menor.
-        while(!pancakeListVerify.equals(pancakeList)){
-
-            // Checar en que parte esta la letra mas grande del pancake
-            for(int i=0; i<=pancakeList.size()-cont; i++){
-                if(pancakeList.get(i) == Collections.max((pancakeListCopy)))
-                    aux = i;
-            }
-
-            // Eliminar la letra maxima del pancake copy
-            for(int i=0; i<=pancakeListCopy.size()-1; i++){
-                if(pancakeListCopy.get(i) == Collections.max((pancakeListCopy)))
-                    pancakeListCopy.remove(i);
-            }
-
-            // Obtenemos una parte del arreglo para darle vuelta
-            for(int i=0; i<=aux; i++){
-                pancakePart.add(pancakeList.get(i));
-            }
-            Collections.reverse(pancakePart);
-
-            // Aqui añadimos la parte volteada
-            for(int i=0; i<=aux; i++){
-                pancakeList.set(i,pancakePart.get(i));
-            }
-            contAux++;
-            System.out.println("Volteada num "+contAux+": "+pancakeList);
-            pancakePart.clear();
-
-            // Se vuelte a tomar la parte volteada y se realiza otro volteada
-            for(int i=0; i<=pancakeList.size()-cont; i++){
-                pancakePart.add(pancakeList.get(i));
-            }
-            Collections.reverse(pancakePart);
-            for(int i=0; i<=pancakeList.size()-cont; i++){
-                pancakeList.set(i,pancakePart.get(i));
-            }
-            contAux++;
-            System.out.println("Volteada num. "+contAux+": "+pancakeList);
-            pancakePart.clear();
-            cont++;
-        }*/
-        
-        System.out.println();
-        System.out.println();
-        System.out.println("- PANCAKE ORDENADO: "+pancakeList);
-        long endTime = System.currentTimeMillis();
-        System.out.println("Total execution time: " + (endTime - startTime));
+        bfs(pancakeList);
     }
 
-    public int BFS(String[] A) {
-        int group = 0;
 
-        boolean[] visited = new boolean[A.length];
-
-        for(int k = 0; k < A.length; k++) {
-            if (visited[k]) {
-                continue;
-            }
-            group++;
-            Queue<String> q = new LinkedList<>();
-            q.offer(A[k]);
-            while(!q.isEmpty()) {
-                String curStr = q.poll();
-                for(int m = 0; m < A.length; m++){
-                    if (visited[m]) {
-                        continue;
-                    }
-                    String nextStr = A[m];
-                    int diff = 0;
-                    for(int i = 0; i < curStr.length(); i++) {
-                        if (curStr.charAt(i) != nextStr.charAt(i)) {
-                            diff++;
-                        }
-                    }
-                    //important here, "aaaaa", "aaaaa" should be grouped together
-                    if (diff == 2 || (diff == 0 && curStr.length() >=2) ) {
-                        visited[m] = true;
-                        q.offer(nextStr);
-                    }
+    public static void bfs(ArrayList<Character> pancake){
+        ArrayList<ArrayList<Character>> adj = new ArrayList<>(); // adjacency list representation
+        int n; // number of nodes
+        Character s; // source vertex
+        LinkedList<Character> q = new LinkedList<Character>();
+        boolean used[] = new boolean[n];
+        int d[] = new int[n];
+        int p[] = new int[n];
+        q.push(s);
+        used[s] = true;
+        p[s] = -1;
+        while (!q.isEmpty()) {
+            int v = q.pop();
+            for (int u : adj.get(v)) {
+                if (!used[u]) {
+                    used[u] = true;
+                    q.push(u);
+                    d[u] = d[v] + 1;
+                    p[u] = v;
                 }
             }
         }
-        return group;
     }
-    /* 
-    private static void BusquedaAmplitud(ArrayList pancakeDesor){
-        String pancake = "";
-        int contador = 0;
-        ArrayList<Integer> D = new ArrayList<Integer>();
-		ArrayList<Integer> P = new ArrayList<Integer>();
-        for(int i=0; i<pancakeDesor.size(); i++){
-            pancake += pancakeDesor.get(i);
-        }
-        Queue<Character> cola = new LinkedList();
-        cola.add(pancakeDesor);
-        System.out.println("String: "+pancake);
-        System.out.println("Cola: "+cola);
-        D.add(0, 0);
-		P.add(0, null);
-
-    }*/
-
 }
