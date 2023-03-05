ef2d34602e35334c038145dcc57aa3fa3fbbab8c
Miguel Rios <91401349+NeoMikhe@users.noreply.github.com>
2023-03-04 20:37:40 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,48 @@
 import java.util.*;
-
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 public class Main {
-    public static void main(String[] args) throws Exception {
-
-        
-        long startTime = System.currentTimeMillis();
-        //List<Character> pancakeList = Arrays.asList("A", "B", "C", "D", "E");
-        ArrayList<Character> pancakeList = new ArrayList<Character>();
-        ArrayList<Character> pancakeListVerify = new ArrayList<Character>();
-        ArrayList<Character> pancakePart = new ArrayList<Character>();
-        ArrayList<Character> pancakeListCopy = new ArrayList<Character>();
-        int cont = 1, aux = 0, contAux = 0;
-        // 65 == A && 91 == Z
-        // CREANDO LA LISTA ABCDE...
-        for(int i=65; i<69; i++){
-            pancakeList.add((char)i);
-            pancakeListCopy.add((char)i);
-            pancakeListVerify.add((char)i);
-        }
-        
-        System.out.println("Pancake ordenado: "+pancakeList);
-        Collections.shuffle(pancakeList);
-        System.out.println("Pancake desornado : "+pancakeList);
-        System.out.println();
-        System.out.println();
-        bfs(pancakeList);
+    public  static void main(String args[]){
+        breadthFirstTraversal("E");
     }
-
-
-    public static void bfs(ArrayList<Character> pancake){
-        ArrayList<ArrayList<Character>> adj = new ArrayList<>(); // adjacency list representation
-        int n; // number of nodes
-        Character s; // source vertex
-        LinkedList<Character> q = new LinkedList<Character>();
-        boolean used[] = new boolean[n];
-        int d[] = new int[n];
-        int p[] = new int[n];
-        q.push(s);
-        used[s] = true;
-        p[s] = -1;
-        while (!q.isEmpty()) {
-            int v = q.pop();
-            for (int u : adj.get(v)) {
-                if (!used[u]) {
-                    used[u] = true;
-                    q.push(u);
-                    d[u] = d[v] + 1;
-                    p[u] = v;
-                }
-            }
+    static class GraphData {
+        public Map<String, LinkedList<String>> adj;
+    
+        GraphData(HashMap<String, LinkedList<String>> m) {
+            adj = m;
         }
+    
+        public Map<String, LinkedList<String>> getAdj() {
+            return adj;
+        }
+    }
+    static void breadthFirstTraversal(String source){
+
+        HashMap<String,LinkedList<String>> map = new HashMap<String,LinkedList<String>>();
+        map.put("A",new LinkedList<String>(){{add("B");add("C");}});
+        map.put("B",new LinkedList<String>(){{add("A");add("B");add("D");}});
+        map.put("C",new LinkedList<String>(){{add("A");add("E");}});
+        map.put("D",new LinkedList<String>(){{add("C");add("E");;add("B");}});
+        map.put("E",new LinkedList<String>(){{add("B");add("D");}});
+        GraphData gd = new GraphData(map);
+
+        HashSet<String> visted = new HashSet<String>();
+
+        LinkedList<String> q = new LinkedList();
+        Stack<String> stack = new Stack<String>();
+        stack.push(source);
+        q.add(source);
+
+        visted.add(source);
+        while(!q.isEmpty()){
+        String  next = q.poll();
+        for(String s: gd.getAdj().get(next)) {
+            if (!visted.contains(s)) {
+                System.out.println("visted Vertex "+s);
+                q.add(s);
+                visted.add(s);
+            }
+        }}
     }
 }
